569b564c50ef3d35ed46c25e7491ef5010a8e8f8
Ian Rodrigues <ianrodrigues107@gmail.com>
2022-11-15 09:48:33 -0300

    Other components

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,12 +1,18 @@
 import NotificationButton from "./components/NotificationButton"
+import Header from "./components/header"
+import SalesCard from "./components/SalesCard"
 
 function App() {
-  return(
+  return (
     <>
-    <h1>
-      ola mundo
-    </h1>
-    <NotificationButton />
+      <Header />
+      <main>
+        <section id="sales">
+          <div className="dsmeta-container">
+            <SalesCard/>
+          </div>
+        </section>
+      </main>
     </>
   )
 }
