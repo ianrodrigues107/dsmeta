14d2dba674429bd4d1cb528a3a38231be0e535fd
Ian Rodrigues <ianrodrigues107@gmail.com>
2022-11-17 13:27:14 -0300

    Twillio SMS

--- backend/src/main/java/com/iandev/dsmeta/controllers/SaleController.java
+++ backend/src/main/java/com/iandev/dsmeta/controllers/SaleController.java
@@ -1,15 +1,19 @@
 package com.iandev.dsmeta.controllers;
 
+import javax.websocket.server.PathParam;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.iandev.dsmeta.entities.Sale;
 import com.iandev.dsmeta.services.SaleService;
+import com.iandev.dsmeta.services.SmsService;
 
 @RestController
 @RequestMapping(value = "/sales")
@@ -17,6 +21,8 @@ public class SaleController {
 
 	@Autowired
 	private SaleService service;
+	@Autowired
+	private SmsService smsService;
 	
 	@GetMapping
 	public Page<Sale> findSales(
@@ -26,4 +32,11 @@ public class SaleController {
 		
 		return service.findSales(minDate, maxDate, pageable);
 	}
+	
+	@GetMapping("/{id}/notification")
+	public void notifySms(@PathVariable Long id) {
+		
+		smsService.sendSms(id);
+	}
+	
 }
